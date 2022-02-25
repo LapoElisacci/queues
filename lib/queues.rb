497181cf09298d7d4ed3e5f6d4c466170724cdd3
Lapo <lapoelisacci@gmail.com>
2022-02-25 19:45:53 +0100

    First commit

---
+++ lib/queues.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "queues/version"
+
+module Queues
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/queues_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Queues do
+  it "has a version number" do
+    expect(Queues::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
