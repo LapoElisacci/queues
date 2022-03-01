c1048ec89e94382bd70bbf84dfd776e42ebaba87
Lapo <lapoelisacci@gmail.com>
2022-03-01 18:24:41 +0100

    Bump version 0.1.0.beta

--- spec/queues_spec.rb
+++ spec/queues_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Queues do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Queues::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
