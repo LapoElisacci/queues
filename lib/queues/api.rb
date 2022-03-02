187d50f562c588d85b3933dec79c881edea16db8
Lapo <lapoelisacci@gmail.com>
2022-03-02 19:57:53 +0100

    Doc

--- lib/queues/api.rb
+++ lib/queues/api.rb
@@ -5,6 +5,12 @@ module Queues
     class << self
       attr_accessor :rabbits
 
+      #
+      # Register Rails Queue RabbitMQ Framework schemas
+      #
+      # @param [Queues::Rabbit::Schema] schema queue-rabbit Schema
+      # @param [String] host RabbitMQ URI
+      #
       def rabbit(schema, host)
         self.rabbits ||= []
         self.rabbits << Queues::Rabbit.configure(schema, host)
